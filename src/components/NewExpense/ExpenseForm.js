20fb97eb6a2e19d93f03fa364483d941fa9ec954
Your Name <behnooda81@gmail.com>
2023-05-04 23:28:23 +0330

    Add NewExpense and ExpenseForm components

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState(' ');
+    const [enteredAmount, setEnteredAmount] = useState(' ');
+    const [enteredDate, setEnteredDate] = useState(' ');
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    };
+
+    return (
+        <form>
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
+                    <lable>Title</lable>
+                    <input type='text' onChange={titleChangeHandler} />
+                </div>
+                <div className='new-expense__control'>
+                    <lable>Amount</lable>
+                    <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler} />
+                </div>
+                <div className='new-expense__control'>
+                    <lable>Date</lable>
+                    <input type='date' min="2019-01-01" onChange={dateChangeHandler} />
+                </div>
+            </div>
+            <div className='new-expense__actions'>
+                <button type='submit'>Add Expenses</button>
+            </div>
+        </form>
+    );
+}
+
+export default ExpenseForm;
